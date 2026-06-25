6fb44f3c10bcb7ab9a7731c0dacce8b0f80d6545
Lucas <257867858+findleyl@users.noreply.github.com>
2026-06-25 11:37:23 -0400

    chore(release): bump to 0.6.86 (vc95)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "org.astermail.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 94
-        versionName = "0.6.85"
+        versionCode = 95
+        versionName = "0.6.86"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
