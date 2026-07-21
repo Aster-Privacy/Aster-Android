381dc92209aeb1e622c7e53f5ed760ed36339e9e
Lucas <257867858+findleyl@users.noreply.github.com>
2026-07-20 22:19:44 -0400

    chore: bump versionCode to 131 (130 already used on Play)
    bump: 0.6.122 (131)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "org.astermail.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 130
-        versionName = "0.6.121"
+        versionCode = 131
+        versionName = "0.6.122"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
