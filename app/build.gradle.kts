5c1d72c2abcfb27ad8551f22039f50aab0ee28e2
Lucas <257867858+findleyl@users.noreply.github.com>
2026-08-15 08:56:52 -0400

    chore(release): 0.6.147 (157)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "org.astermail.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 156
-        versionName = "0.6.146"
+        versionCode = 157
+        versionName = "0.6.147"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
